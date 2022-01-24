8cafec56a8b90ce6e39a16defda20f78cd528a8d
gadwa <gadwalasrinivas559@gmail.com>
2022-01-24 20:24:50 +0530

    first commit

---
+++ src/test/java/seleniumproject/MavenProject/SampleTest.java
@@ -0,0 +1,20 @@
+package seleniumproject.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+
+	@Test
+	public void mobileTestCase() {
+		// TODO Auto-generated method stub
+		System.out.println("mobileTestCase");
+	}
+	
+	@Test
+	public void mobileSampleTest() {
+		// TODO Auto-generated method stub
+		System.out.println("mobileSampleTest");
+	}
+}
+
---
+++ src/test/java/seleniumproject/MavenProject/SeleniumTest.java
@@ -0,0 +1,18 @@
+package seleniumproject.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void SeleniumTestCase() {
+		// TODO Auto-generated method stub
+		System.out.println("SeleniumTestCase");
+	}
+	
+	@Test
+	public void SeleniumSampleTest() {
+		// TODO Auto-generated method stub
+		System.out.println("SeleniumSampleTest");
+	}
+}
